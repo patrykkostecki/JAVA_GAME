2e6b3a672ff52c0ff319948498ce112115d4f9aa
Szymon Czermak <SzyCzermak00@gmail.com>
2023-12-27 00:21:17 +0100

    Czwarta

--- GameMap/src/Main.java
+++ GameMap/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("SZYMON CZERNIAK CWEL ESSA");
+        System.out.println("Patryk Kostecki CWEL ESSA");
     }
 
 }
